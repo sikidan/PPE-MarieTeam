6ad1af286e6c6277fe2f1a31d9f7f02c9451b04c
Sikidan <remy.bouchez.eleve@gmail.com>
2018-11-29 09:54:00 +0100

    Modification reservation + navbar

--- nav-bar.php
+++ nav-bar.php
@@ -6,9 +6,10 @@
 							<div id="colorlib-logo"><a href="index.php">MarieTeam</a></div>
 						</div>
 						<div class="col-xs-10 text-right menu-1">
+						<?php $page = basename($_SERVER['PHP_SELF']); ?>
 							<ul>
-								<li class="active"><a href="index.php">Accueil</a></li>
-								<li class="has-dropdown">
+							<li class="<?php if($page == 'index.php'){ echo ' active"';}?>"><a href="index.php" >Accueil</a></li>
+								<li class="has-dropdown" class="<?php if($page == 'offres.php'){ echo ' active"';}?>">
 									<a href="offres.php">Nos offres</a>
 									<ul class="dropdown">
 										<li><a href="#">Promotions</a></li>
@@ -17,10 +18,10 @@
 										<li><a href="#">Transport de passagers</a></li>
 									</ul>
 								</li>
-								<li><a href="tarif.php">Tarif</a></li>
-								<li><a href="reservationVoyageur.php">Réservation</a></li>
-								<li><a href="connexion.php">Connexion/Inscription</a></li>
-								<li><a href="contact.php">Contact</a></li>
+								<li class="<?php if($page == 'tarif.php'){ echo ' active"';}?>"><a href="tarif.php" >Tarif</a></li>
+								<li class="<?php if($page == 'reservationVoyageur.php'){ echo ' active';}?>"><a href="reservationVoyageur.php" >Réservation</a></li>
+								<li class="<?php if($page == 'connexion.php'){ echo ' active"';}?>"><a href="connexion.php" >Connexion/Inscription</a></li>
+								<li class="<?php if($page == 'contact.php'){ echo ' active"';}?>"><a href="contact.php" >Contact</a></li> 								
 							</ul>
 						</div>
 					</div>
