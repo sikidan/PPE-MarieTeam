424f9af7db217e74fe273087017f79bb002a2d54
Deny <d.vanquickelberge@hotmail.com>
2019-01-29 10:27:26 +0100

    Tentative correction page contact 2.0

--- sendContact.php
+++ sendContact.php
@@ -1,41 +1,41 @@
 <?php
 //Permet de démarrer une session visiteur
 session_start();
-    $errors = array(); //Création d'une vérification des champs
-if(!array_key_exists('nom', $_POST) || $_POST['nom'] == '') { //Verification de l'existence du champ
-    $errors ['nom'] = "Vous n'avez pas renseigné votre nom";
+
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
+    $passage_ligne = "\r\n";
 }
-if(!array_key_exists('prenom', $_POST) || $_POST['prenom'] == '') { //Verification de l'existence du champ
-    $errors ['prenom'] = "Vous n'avez pas renseigné votre prénom";
-}
-if(!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { //On vérifie l'existence
-    $errors ['mail'] = "Vous n'avez pas renseigné votre email";
-}
-if(!array_key_exists('sujet', $_POST) || $_POST['sujet'] == '') { //Verification de l'existence du champ
-    $errors ['sujet'] = "Vous n'avez pas renseigné de sujet";
-}
-if(!array_key_exists('message', $_POST) || $_POST['message'] == '') { //Verification de l'existence du champ
-    $errors ['message'] = "Vous n'avez pas renseigné votre message";
+else {
+    $passage_ligne = "\n";
 }
 
-//Verification des infos transmises lors de la vérification
-if(!empty($errors)){ // si erreur on renvoie vers la page précédente
-  $_SESSION['errors'] = $errors;//on stocke les erreurs
-  $_SESSION['inputs'] = $_POST;
-  header('Location: contact.php');
-  }else{
-  $_SESSION['success'] = 1;
-  
-    $from = $_POST['email'];
-    
-    $to = 'marieteam.contact@gmail.com';
-        
-    $subject = $_POST['sujet'];
-        
-    $message = $_POST['message'];
-    
-    $headers = "From:" . $from;
-    
-    mail($to, $subject, $message, $headers);
-  }
+//Déclaration du message
+$message_texte = $_POST['message'];
+
+//Création d'un boundary
+$boundary = "-----=".md5(rand());
+
+//Création d'un sujet
+$sujet = $_POST['sujet'];
+
+//Création du header de l'email
+$header = 'From: \"Client\"<'$_POST['email']'>'.$passage_ligne;
+$header .= "Reply-to: \"MarieTeam\" <marieteam.contact@gmail.com>".$passage_ligne;
+$header .= "MIME-Version: 1.0".$passage_ligne;
+$header .= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+
+//Création du message
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+
+//Ajout du message
+$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+
+//Envoi de l'email
+mail($mail,$sujet,$message,$header);
+
 ?>
